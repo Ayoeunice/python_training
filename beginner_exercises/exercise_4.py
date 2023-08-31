cb907c66ff056e2c898d451c19c2d10a6e38a060
Ayoeunice <euniceayo100@gmail.com>
2023-08-31 23:25:35 +0300

    exercise 4

--- beginner_exercises/exercise_4.py
+++ beginner_exercises/exercise_4.py
@@ -1,4 +1,4 @@
 # Display float number with 2 decimal places using print() Given: num=458.541315
 number = 458.541315
 rounded_number = round(number, 2)
-print("Rounding 458.541315 up to two decimal places:",  rounded_number)
+print(rounded_number)
