c7a4f2b5f66e16d7d3deafa7348c78ca0b350af4
Ayoeunice <euniceayo100@gmail.com>
2023-08-31 23:01:31 +0300

    exercise 3

--- beginner_exercises/exercise_3.py
+++ beginner_exercises/exercise_3.py
@@ -1,4 +1,4 @@
-#  Convert Decimal number to octal using print() output formatting Given:
+#  Convert Decimal number to octal using print() output formatting Given: num=8
 dec = 8
 print("The decimal value of", dec, "is:")
 print(oct(dec), "in octal.")
