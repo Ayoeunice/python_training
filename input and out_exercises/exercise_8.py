e7268af1e21dfcc52e8330e70f2b646d02f55535
Ayoeunice <euniceayo100@gmail.com>
2023-09-06 08:32:35 +0300

    exercise 8

---
+++ input and out_exercises/exercise_8.py
@@ -0,0 +1,8 @@
+#  Format variables using a string.format() method.
+
+formatted1 = "totalMoney{money}".format(money="=1000")
+formatted2 = "quantity{quantity}".format(quantity="=3")
+formatted3 = "price{price}".format(price="=450")
+print(formatted1)
+print(formatted2)
+print(formatted3)
