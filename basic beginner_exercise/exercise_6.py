e1aa38436ccf803c2674a27ee984db380056e1a2
Ayoeunice <euniceayo100@gmail.com>
2023-09-09 15:57:12 +0300

    exercise 6

---
+++ basic beginner_exercise/exercise_6.py
@@ -0,0 +1,10 @@
+# Display numbers divisible by 5 from a list
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+# Define a list of numbers
+numbers = [10, 20, 33, 46, 35, 55]
+
+# Iterate through the list and print numbers divisible by 5
+for num in numbers:
+    if num % 5 == 0:
+
+     print(num)
