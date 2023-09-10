3112bd121ce43dca3ab3a1ff9de2fb28d997656f
Ayoeunice <euniceayo100@gmail.com>
2023-09-10 17:20:58 +0300

    exercise 3

---
+++ loop_exercise/exercise_3.py
@@ -0,0 +1,18 @@
+# Calculate the sum of all numbers from 1 to a given number
+# Write a program to accept a number from a user and calculate the sum of all numbers from 1 to a given number
+
+try:
+    num = int(input("Enter a positive integer: "))
+
+    if num < 1:
+        print("Please enter a positive integer.")
+    else:
+        total = 0
+
+        for i in range(1, num + 1):
+            total += i
+
+        print(f"The sum of all numbers from 1 to {num} is: {total}")
+
+except ValueError:
+    print("Invalid input. Please enter a positive integer.")
