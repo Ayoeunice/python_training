f4f680d00c2cf031286087a97244cfca351b1670
Ayoeunice <euniceayo100@gmail.com>
2023-09-10 19:38:10 +0300

    exercise 18

---
+++ loop_exercise/exercise_18.py
@@ -0,0 +1,23 @@
+# Print the following pattern
+# Write a program to print the following start pattern using the for loop
+#
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+
+rows = 5
+for i in range(1, rows + 1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+
+for i in range(rows - 1, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
