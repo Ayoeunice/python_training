587a49ed508491a65f5bbd6d8b9f467d401136e6
Ayoeunice <euniceayo100@gmail.com>
2023-09-20 11:28:46 +0300

    exercise 1

---
+++ function_exercise/exercise_1.py
@@ -0,0 +1,10 @@
+# Create a function in Python
+# Write a program to create a function that takes two arguments, name and age, and print their value.
+
+def print_name_and_age(name, age):
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print_name_and_age(name, age)
