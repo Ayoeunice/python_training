84fdde45aacab2a7bb276ec42f2f18f4fc4847b3
Ayoeunice <euniceayo100@gmail.com>
2023-09-22 11:06:54 +0300

    exercise 9

---
+++ function_exercise/exercise_9.py
@@ -0,0 +1,7 @@
+# Find the largest item from a given list
+# x = [4, 6, 8, 24, 12, 2]
+x = [4, 6, 8, 24, 12, 2]
+largest_item = max(x)
+# print("The largest item in the list is:", largest_item)
+print(largest_item)
+
