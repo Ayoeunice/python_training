43bb648a2e7f5eb9c43d1904a2a8b5b98df43ce6
Ayoeunice <euniceayo100@gmail.com>
2023-09-22 12:22:47 +0300

    exercise 6

--- input and out_exercises/exercise_6.py
+++ input and out_exercises/exercise_6.py
@@ -1,30 +1,52 @@
 # Write all content of a given file into a new file by skipping line number 5
-# Specify the input and output file names
-input_file_name = 'test.txt'  # Replace with your input file name
-output_file_name = 'test1.txt'  # Replace with your output file name
+# Create a test.txt file and add the below content to it.
+# Given test.txt file:
 
-try:
-    # Open the input file for reading
-    with open('test.txt', 'r') as input_file:
-        # Read all lines from the input file into a list
-        lines = input_file.readlines()
-        # Check if there are at least 5 lines in the file
-        if len(lines) >= 5:
-            # Remove line 5 (index 4) from the list
-            del lines[4]
+# line1
+# line2
+# line3
+# line4
+# line5
+# line6
+# line7
+# Expected Output: new_file.txt
+# line1
+# line2
+# line3
+# line4
+# line6
+# line7
 
-            # Open the output file for writing
-            with open('test1.txt', 'w') as output_file:
-                # Write the modified content (without line 5) to the output file
-                output_file.writelines(lines)
+# Open the original file for reading
+#with open('test.txt', 'r') as original_file:
+    # Create a new file for writing
+   # with open('test.txt', 'w') as new_file:
+        # Initialize a line counter
+        #line_count = 0
 
-            print(f"Content from '{input_file}' copied to '{output_file}' with line 5 skipped.")
-        else:
-            print(f"The input file '{input_file}' does not have at least 5 lines.")
-except FileNotFoundError:
-    print(f"The input file '{input_file}' was not found.")
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+        # Iterate over each line in the original file
+        #for line in original_file:
+            # Increment the line counter
+            #line_count += 1
+
+            # Skip line number 5
+            #if line_count != 5:
+                # Write the line to the new file
+                #new_file.write(line)
 
 
+# Open the original file for reading
+with open('test.txt', 'r') as original_file:
+    # Read all the lines from the original file
+    lines = original_file.readlines()
 
+# Create a new file for writing
+with open('new_file.txt', 'w') as new_file:
+    # Write all lines except the 5th line (index 4)
+    for i, line in enumerate(lines):
+        if i != 4:
+            new_file.write(line)
+
+# Print a message to indicate the process is completed
+print("Content copied to new_file.txt with line 5 skipped.")
+print(new_file)
