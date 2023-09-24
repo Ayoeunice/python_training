0beb632b95eb06124149172347eb410691d32023
Ayoeunice <euniceayo100@gmail.com>
2023-09-24 10:55:40 +0300

    exercise 11

---
+++ string_exercise/exercise_11.py
@@ -0,0 +1,13 @@
+# Reverse a given string
+ # Given:
+# # str1 = "PYnative"
+
+str1 = "PYnative"
+
+# Use string slicing to reverse the string
+reversed_str1 = str1[::-1]
+
+#print("Reversed string:", reversed_str1)
+
+print(reversed_str1)
+
