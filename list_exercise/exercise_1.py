202d72bc8cfe03f016df431ad39919adc6da3ae9
Ayoeunice <euniceayo100@gmail.com>
2023-09-25 20:52:00 +0300

    exercise 1

---
+++ list_exercise/exercise_1.py
@@ -0,0 +1,11 @@
+# Reverse a list in Python
+# Given:
+# list1 = [100, 200, 300, 400, 500]
+
+list1 = [100, 200, 300, 400, 500]
+
+# Use the reverse() method to reverse the list in-place
+list1.reverse()
+
+# Print the reversed list
+print(list1)
