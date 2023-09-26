b5c44167ef38d7218379782969f3ce9c50a15ca5
Ayoeunice <euniceayo100@gmail.com>
2023-09-26 16:09:44 +0300

    exercise 6

---
+++ set_exercise/exercise_6.py
@@ -0,0 +1,21 @@
+# Return a set of elements present in Set A or B, but not both
+# Given:
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+# Return a set containing elements present in set1 or set2, but not both, using the ^ operator
+result_set = set1 ^ set2
+
+# Print the result set
+print(result_set)
+
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+
+# Return a set containing elements present in set1 or set2, but not both, using symmetric_difference()
+# result_set = set1.symmetric_difference(set2)
+
+# Print the result set
+# print(result_set)
