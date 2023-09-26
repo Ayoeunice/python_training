947a6caa3f4a9a746480637f0cc0b375ce321103
Ayoeunice <euniceayo100@gmail.com>
2023-09-26 15:25:06 +0300

    exercise 2

---
+++ set_exercise/exercise_2.py
@@ -0,0 +1,21 @@
+# Return a new set of identical items from two sets
+# Given:
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+set1 = {10, 20, 30, 40, 50}
+set2 = {30, 40, 50, 60, 70}
+
+# Find the set of identical items using the intersection() method
+identical_items = set1.intersection(set2)
+
+# Print the set of identical items
+print(identical_items)
+
+# set1 = {10, 20, 30, 40, 50}
+# set2 = {30, 40, 50, 60, 70}
+
+# Find the set of identical items using the & operator
+# identical_items = set1 & set2
+
+# Print the set of identical items
+# print(identical_items)
