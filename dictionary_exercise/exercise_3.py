ad4741f2e74f75b15a4ad1fa9f06655256b318c8
Ayoeunice <euniceayo100@gmail.com>
2023-09-26 13:40:41 +0300

    exercise 3

---
+++ dictionary_exercise/exercise_3.py
@@ -0,0 +1,31 @@
+# Print the value of key ‘history’ from the below dict
+# sampleDict = {
+#     "class": {
+#         "student": {
+#             "name": "Mike",
+#             "marks": {
+#                 "physics": 70,
+#                 "history": 80
+#             }
+#         }
+#     }
+# }
+
+sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+            }
+        }
+    }
+}
+
+# Access the value of 'history' key
+history_score = sampleDict["class"]["student"]["marks"]["history"]
+
+# Print the value of 'history' key
+print("History score:", history_score)
+
