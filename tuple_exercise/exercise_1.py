64ecbbff37f32058d6d39e58dc27c9cd44c2322f
Ayoeunice <euniceayo100@gmail.com>
2023-09-26 22:15:41 +0300

    exercise 1

---
+++ tuple_exercise/exercise_1.py
@@ -0,0 +1,11 @@
+# Reverse the tuple
+# Given: tuple1 = (10, 20, 30, 40, 50)
+
+tuple1 = (10, 20, 30, 40, 50)
+
+# Reverse the tuple using slicing
+reversed_tuple = tuple1[::-1]
+
+# Print the reversed tuple
+print(reversed_tuple)
+
