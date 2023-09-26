e8865c29085d23d01c24bee476da260437e149c2
Ayoeunice <euniceayo100@gmail.com>
2023-09-26 22:50:29 +0300

    exercise 6

---
+++ tuple_exercise/exercise_6.py
@@ -0,0 +1,11 @@
+# Copy specific elements from one tuple to a new tuple
+# Write a program to copy elements 44 and 55 from the following tuple into a new tuple.
+# Given:
+# tuple1 = (11, 22, 33, 44, 55, 66)
+tuple1 = (11, 22, 33, 44, 55, 66)
+
+# Copy elements 44 and 55 into a new tuple
+new_tuple = tuple1[3:5]
+
+# Print the new tuple
+print(new_tuple)
