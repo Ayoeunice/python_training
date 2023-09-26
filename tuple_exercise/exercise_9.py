8a62bc872ea743dfd467774b78266bb0bd99fa28
Ayoeunice <euniceayo100@gmail.com>
2023-09-26 23:02:16 +0300

    exercise 9

---
+++ tuple_exercise/exercise_9.py
@@ -0,0 +1,11 @@
+# Counts the number of occurrences of item 50 from a tuple
+# Given:
+# tuple1 = (50, 10, 60, 70, 50)
+tuple1 = (50, 10, 60, 70, 50)
+
+# Count the number of occurrences of item 50 in the tuple
+count_50 = tuple1.count(50)
+
+# Print the count
+# print("Number of occurrences of 50:", count_50)
+print(count_50)
