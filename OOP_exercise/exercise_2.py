d1cf0507b4ccece10a66d70e84cf61d33621d509
Ayoeunice <euniceayo100@gmail.com>
2023-09-27 22:03:33 +0300

    exercise 2

---
+++ OOP_exercise/exercise_2.py
@@ -0,0 +1,13 @@
+# Create a Vehicle class without any variables and methods
+class Vehicle:
+    pass
+
+# Create an instance of the Vehicle class
+car = Vehicle()
+
+# You can create more instances of the Vehicle class
+bus = Vehicle()
+motorcycle = Vehicle()
+
+
+
