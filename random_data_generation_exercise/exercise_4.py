fddf5a0757c72e3f4094534b0289ed39525d387c
Ayoeunice <euniceayo100@gmail.com>
2023-09-30 18:05:06 +0300

    exercise 4

---
+++ random_data_generation_exercise/exercise_4.py
@@ -0,0 +1,11 @@
+# Pick a random character from a given String
+import random
+
+# Given string
+string = "Jesus, Christ"
+
+# Pick a random character from the string
+random_character = random.choice(string)
+
+print("Random character:", random_character)
+
